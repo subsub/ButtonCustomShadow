b33e445c9f3e708a653f1f35b375dbc32d880ec7
a.artemov <a.artemov@kernel.ua>
2018-10-01 11:00:19 +0300

    Code refactor for optimal Kotlin usage

--- app/src/main/java/com/subsub/buttoncustomshadow/MainActivity.kt
+++ app/src/main/java/com/subsub/buttoncustomshadow/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.subsub.buttoncustomshadow
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button1.setOnClickListener {
-            Toast.makeText(this, "Button1 Clicked", Toast.LENGTH_SHORT).show()
-        }
-        button2.setOnClickListener {
-            Toast.makeText(this, "Button2 Clicked", Toast.LENGTH_SHORT).show()
-        }
-        button3.setOnClickListener {
-            Toast.makeText(this, "Button3 Clicked", Toast.LENGTH_SHORT).show()
+        listOf(button1, button2, button3).onEach { it.setOnClickListener(this) }
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!) {
+            button1 -> showToast("Button1 Clicked")
+            button2 -> showToast("Button2 Clicked")
+            button3 -> showToast("Button3 Clicked")
         }
     }
+
+    private fun Activity.showToast(string: String) =
+            Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
 }
