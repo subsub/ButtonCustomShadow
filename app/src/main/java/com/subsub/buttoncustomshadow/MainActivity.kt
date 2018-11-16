3b5f840e511b86f1d93f08aed89a0a88a9a1aff2
Subkhan Sarif <subkhansarif@gmail.com>
2018-11-16 18:45:39 +0700

    - Use FrameLayout instead of TextView for base BeautyView - Add BeautyLayout - Add shadow control to sample app

--- app/src/main/java/com/subsub/buttoncustomshadow/MainActivity.kt
+++ app/src/main/java/com/subsub/buttoncustomshadow/MainActivity.kt
@@ -1,29 +1,88 @@
 package com.subsub.buttoncustomshadow
 
-import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Toast
+import android.view.View.GONE
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+
+    var viewElevation: Float = 0f
+    var viewShadowRadius: Float = 0f
+    var viewXOffset: Float = 0f
+    var viewYOffset: Float = 0f
+    var viewShadowHeight: Float = 0f
+    var viewShadowWidth: Float = 0f
+
+    var listener: (() -> Unit)? = null
+
+    var controlContainer: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        listOf(button1, button2, button3).onEach { it.setOnClickListener(this) }
+        controlContainer = control_container
+        controlContainer?.visibility = GONE
+
+        listOf(elevation,
+                shadow_radius,
+                x_offset,
+                y_offset,
+                shadow_height,
+                shadow_width)
+                .onEach { it.setOnSeekBarChangeListener(this) }
+                .onEach {
+                    it.max = 100
+                }
     }
 
-    override fun onClick(v: View?) {
-        when (v!!) {
-            button1 -> showToast("Button1 Clicked")
-            button2 -> showToast("Button2 Clicked")
-            button3 -> showToast("Button3 Clicked")
+    fun reset() {
+        listener = null
+        viewElevation = 0f
+        viewShadowRadius = 0f
+        viewXOffset = 0f
+        viewYOffset = 0f
+        viewShadowHeight = 0f
+        viewShadowWidth = 0f
+    }
+
+    fun applyCurrent(elevation: Float, shadow_radius: Float, x_offset: Float, y_offset: Float, shadow_height: Float, shadow_width: Float) {
+        viewElevation = elevation
+        viewShadowRadius = shadow_radius
+        viewXOffset = x_offset
+        viewYOffset = y_offset
+        viewShadowHeight = shadow_height
+        viewShadowWidth = shadow_width
+        this.elevation.progress = viewElevation.toInt()
+        this.shadow_radius.progress = viewShadowRadius.toInt()
+        this.x_offset.progress = viewXOffset.toInt()
+        this.y_offset.progress = viewYOffset.toInt()
+        this.shadow_height.progress = (viewShadowHeight * 100).toInt()
+        this.shadow_width.progress = (viewShadowWidth * 100).toInt()
+    }
+
+    override fun onProgressChanged(view: SeekBar?, progress: Int, p2: Boolean) {
+        if (view != null) with(view) {
+            when (id) {
+                R.id.elevation -> viewElevation = progress.toFloat()
+                R.id.shadow_radius -> viewShadowRadius = progress.toFloat()
+                R.id.x_offset -> viewXOffset = progress.toFloat()
+                R.id.y_offset -> viewYOffset = progress.toFloat()
+                R.id.shadow_height -> viewShadowHeight = progress.toFloat() / 100f
+                R.id.shadow_width -> viewShadowWidth = progress.toFloat() / 100f
+            }
         }
+        listener?.invoke()
     }
 
-    private fun Activity.showToast(string: String) =
-            Toast.makeText(this, string, Toast.LENGTH_SHORT).show()
+    override fun onStartTrackingTouch(view: SeekBar?) {
+
+    }
+
+    override fun onStopTrackingTouch(view: SeekBar?) {
+
+    }
 }
---
+++ app/src/main/java/com/subsub/buttoncustomshadow/fragments/HomeFragment.kt
@@ -0,0 +1,46 @@
+package com.subsub.buttoncustomshadow.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.subsub.buttoncustomshadow.MainActivity
+import com.subsub.buttoncustomshadow.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+/**
+ * Created by subkhansarif on 14/11/18
+ **/
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val activity = activity as MainActivity
+        btn_button.setOnClickListener {
+            activity.reset()
+            activity.controlContainer?.visibility = VISIBLE
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_buttonFragment)
+        }
+
+        btn_layout.setOnClickListener {
+            activity.reset()
+            activity.controlContainer?.visibility = VISIBLE
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_layoutFragment)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val activity = activity as MainActivity
+        activity.controlContainer?.visibility = GONE
+    }
+}
